cd9769b1e7c78a53cdd53c19769870c1872bffb9
titas1111 <titasaleksandravicius@gmail.com>
2020-01-06 10:54:41 +0200

    update

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask
 from flask import render_template
 from flask import request
 import model
+import pandas as pd
 
 app = Flask(__name__)
+
+df = pd.DataFrame({'A': [0, 1, 2, 3, 4],
+                   'X': [5, 6, 7, 8, 9]})
 
 @app.route("/", methods=['GET', 'POST'])
 def index():    
@@ -12,7 +16,7 @@ def index():
         prediction = model.predictPrice(website)
         modelPrediction = int(prediction[0])
         price = int(prediction[1].replace(" €",""))
-        return render_template("index.html", modelPrediction = modelPrediction, website = website, price = price, diff = price - modelPrediction)
+        return render_template("index.html", modelPrediction = modelPrediction, website = website, price = price, diff = price - modelPrediction, tables=[df.to_html(classes='table')], titles=df.columns.values)
     else:
         return render_template("index.html", modelPrediction = 0)
 
